0f630342f90ba0e18477a65c30579f57e13b7022
Irvin Dinkins <irvdude44@gmail.com>
2023-07-17 20:40:18 -0400

    passed

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
+    def sit(self):
+        print("The dog is sitting.")
+
     pass
+
+fido = Dog()
+fido.bark()
+
+snoopy = Dog()
+snoopy.bark()
+
+fido.sit()
+snoopy.sit()
--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,12 @@ class Person:
     # Class body goes here
 
     #Instance method definition
+    def talk(self):
+        print("Hello World!")
     pass
+
+    def walk(self):
+        print("The person is walking.")
+
+harold = Person()
+harold.talk()
